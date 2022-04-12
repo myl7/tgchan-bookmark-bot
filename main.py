bf79af3f672f462f33a5e4028c7757388b4b2834
myl7 <myl@myl.moe>
2022-04-12 22:08:53 +0800

    Add tg sending

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from time import sleep
 
 import requests
 import dotenv
+import telegram
 
 dotenv.load_dotenv()
 
@@ -17,15 +18,33 @@ logging.basicConfig(level=logging.INFO)
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 if BOT_TOKEN is None:
     logging.critical('BOT_TOKEN not set')
+    exit(1)
 XBEL_URL = os.getenv('XBEL_URL')
 if XBEL_URL is None:
     logging.critical('XBEL_URL not set')
+    exit(1)
 DB_PATH = os.getenv('DB_PATH')
 if DB_PATH is None:
     DB_PATH = 'db.sqlite3'
 POLL_INTERVAL = os.getenv('POLL_INTERVAL')  # Seconds
 if POLL_INTERVAL is None:
     POLL_INTERVAL = 360
+CHAN_NAME = os.getenv('CHAN_NAME')
+if CHAN_NAME is None:
+    logging.critical('CHAN_NAME not set')
+    exit(1)
+elif not CHAN_NAME.startswith('@'):
+    CHAN_NAME = f'@{CHAN_NAME}'
+MSG_TEMPLATE = os.getenv('MSG_TEMPLATE')
+if MSG_TEMPLATE is None:
+    MSG_TEMPLATE = 'New bookmark: {title} {url}'
+elif MSG_TEMPLATE.find('{title}') == -1 or MSG_TEMPLATE.find('{title}') == -1:
+    logging.critical('MSG_TEMPLATE must contain {title} and {url}')
+    exit(1)
+
+bot = telegram.Bot(token=BOT_TOKEN)
+me = bot.get_me()
+logging.info(f'Bot {me.username} started')
 
 
 class AppException(Exception):
@@ -76,6 +95,11 @@ def check_bookmark(cursor, bookmark):
     return row is not None
 
 
+def send_msg(bookmark):
+    text = MSG_TEMPLATE.format(title=bookmark['title'], url=bookmark['url'])
+    bot.send_message(CHAN_NAME, text)
+
+
 def main():
     cursor = get_db()
     init_db(cursor)
@@ -84,8 +108,9 @@ def main():
             bookmarks = get_xbel()
             for bookmark in bookmarks:
                 if not check_bookmark(cursor, bookmark):
+                    send_msg(bookmark)
                     save_bookmark(cursor, bookmark)
-                    logging.info(f'Saved bookmark: {bookmark["title"]}')
+                    logging.info(f'Sent and saved bookmark: {bookmark["title"]}')
         except Exception as e:
             logging.error(f'Error: {e}')
         sleep(POLL_INTERVAL)

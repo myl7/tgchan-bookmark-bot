fedf01e2a39d5d2553c9169f1733278adaac8385
myl7 <myl@myl.moe>
2023-06-09 19:05:23 +0800

    Migrate to async version
    Also add an option DRY_RUN.
    And Fixed validation of MSG_TEMPLATE.

--- main.py
+++ main.py
@@ -4,12 +4,12 @@
 import os
 import xml.etree.ElementTree as ET
 import logging
-import sqlite3
-from time import sleep
+import asyncio
 
-import requests
 import dotenv
 import telegram
+import aiosqlite
+import aiohttp
 
 dotenv.load_dotenv()
 
@@ -38,87 +38,89 @@ elif not CHAN_NAME.startswith('@'):
 MSG_TEMPLATE = os.getenv('MSG_TEMPLATE')
 if MSG_TEMPLATE is None:
     MSG_TEMPLATE = 'New bookmark: {title} {url}'
-elif MSG_TEMPLATE.find('{title}') == -1 or MSG_TEMPLATE.find('{title}') == -1:
+elif MSG_TEMPLATE.find('{title}') == -1 or MSG_TEMPLATE.find('{url}') == -1:
     logging.critical('MSG_TEMPLATE must contain {title} and {url}')
     exit(1)
+DRY_RUN = os.getenv('DRY_RUN')
 
-bot = telegram.Bot(token=BOT_TOKEN)
-me = bot.get_me()
-logging.info(f'Bot {me.username} started')
+SCHEMA = '''
+CREATE TABLE IF NOT EXISTS bookmarks (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  url TEXT NOT NULL UNIQUE,
+  title TEXT NOT NULL,
+  bid INTEGER NOT NULL
+);
+'''
 
 
 class AppException(Exception):
     pass
 
 
-def get_xbel():
-    res = requests.get(XBEL_URL)
-    if res.status_code != 200:
-        raise AppException(f'Req XBEL failed with status code {res.status_code}')
-    body = res.content.decode()
-    root = ET.fromstring(body)
-    if root.tag != 'xbel':
-        raise AppException(f'Invalid XBEL: Expected <xbel> but got <{root.tag}>')
-    bookmarks = [child for child in root if child.tag == 'bookmark']
-    logging.debug(f'Got {len(bookmarks)} bookmarks')
-    return [{
-        'url': bookmark.attrib['href'],
-        'title': bookmark[0].text if bookmark[0].text else '<no title>',
-        'bid': int(bookmark.attrib['id'])
-    } for bookmark in bookmarks]
+async def get_xbel():
+    async with aiohttp.ClientSession() as session:
+        async with session.get(XBEL_URL) as res:
+            if res.status != 200:
+                raise AppException(f'Req XBEL failed with status code {res.status}')
+            body = await res.text()
+            root = ET.fromstring(body)
+            if root.tag != 'xbel':
+                raise AppException(f'Invalid XBEL: Expected <xbel> but got <{root.tag}>')
+            bookmarks = [child for child in root if child.tag == 'bookmark']
+            logging.debug(f'Got {len(bookmarks)} bookmarks')
+            return [{
+                'url': bookmark.attrib['href'],
+                'title': bookmark[0].text if bookmark[0].text else '<no title>',
+                'bid': int(bookmark.attrib['id'])
+            } for bookmark in bookmarks]
 
 
-def get_db():
-    conn = sqlite3.connect(DB_PATH, isolation_level=None)
-    cursor = conn.cursor()
-    return cursor
-
-
-def init_db(cursor):
-    with open('schema.sql') as f:
-        cursor.execute(f.read())
-
-
-def save_bookmark(cursor, bookmark):
-    cursor.execute(
+async def save_bookmark(db, bookmark):
+    await db.execute(
         'INSERT INTO bookmarks (url, title, bid) VALUES (?, ?, ?)',
         (bookmark['url'], bookmark['title'], bookmark['bid'])
     )
 
 
-def check_bookmark(cursor, bookmark):
-    cursor.execute(
+async def check_bookmark(db, bookmark):
+    async with db.execute(
         'SELECT * FROM bookmarks WHERE url = ?',
         (bookmark['url'],)
-    )
-    row = cursor.fetchone()
-    return row is not None
+    ) as cursor:
+        row = await cursor.fetchone()
+        return row is not None
 
 
-def send_msg(bookmark):
+async def send_msg(bot: telegram.Bot, bookmark):
     text = MSG_TEMPLATE.format(title=bookmark['title'], url=bookmark['url'])
-    bot.send_message(CHAN_NAME, text)
+    if DRY_RUN:
+        logging.info(f'Dry run: To send {text}')
+    else:
+        await bot.send_message(CHAN_NAME, text)
 
 
-def main():
-    cursor = get_db()
-    init_db(cursor)
-    while True:
-        try:
-            bookmarks = get_xbel()
-            for bookmark in bookmarks:
-                if not check_bookmark(cursor, bookmark):
-                    send_msg(bookmark)
-                    save_bookmark(cursor, bookmark)
-                    logging.info(f'Sent and saved bookmark: {bookmark["title"]}')
-        except sqlite3.Error as e:
-            # Critical
-            logging.error(f'SQLite3 Error: {e}')
-            exit(1)
-        except Exception as e:
-            logging.error(f'Error: {e}')
-        sleep(POLL_INTERVAL)
+async def main():
+    bot = telegram.Bot(token=BOT_TOKEN)
+    me = await bot.get_me()
+    logging.info(f'Bot {me.username} started')
+    async with aiosqlite.connect(DB_PATH, isolation_level=None) as db:
+        await db.execute(SCHEMA)
+        while True:
+            try:
+                bookmarks = await get_xbel()
+                for bookmark in bookmarks:
+                    if not await check_bookmark(db, bookmark):
+                        await send_msg(bot, bookmark)
+                        await save_bookmark(db, bookmark)
+                        logging.info(f'Sent and saved bookmark: {bookmark["title"]}')
+            except aiosqlite.Error as e:
+                # Critical
+                logging.error(f'SQLite3 Error: {e}')
+                exit(1)
+            except Exception as e:
+                logging.error(f'Error: {e}')
+            await asyncio.sleep(POLL_INTERVAL)
 
 
 if __name__ == '__main__':
-    main()
+    asyncio.run(main())
